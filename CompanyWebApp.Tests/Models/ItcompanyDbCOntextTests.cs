d9c55ae6f63fff5a73b95bc3c43240efcb9c3fb6
Matthew Kovalyk <matviigame@gmail.com>
2024-05-02 12:16:17 +0300

    test: add structure of tests

---
+++ CompanyWebApp.Tests/Models/ItcompanyDbCOntextTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using Microsoft.EntityFrameworkCore;
+using CompanyWebApp.Models;
+
+namespace CompanyWebApp.Tests
+{
+    [TestFixture]
+    public class DbContextTests
+    {
+        private DbContextOptions<ItcompanyDbContext> _options;
+
+        [SetUp]
+        public void Setup()
+        {
+            _options = new DbContextOptionsBuilder<ItcompanyDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            using (var context = new ItcompanyDbContext(_options))
+            {
+                // Create a new Country entity
+                var newCountry = new Country
+                {
+                    Name = "Test Country",
+                    Continent = "Test Continent",
+                    Type = "Test Type",
+                    Gdp = 100, // Example value for Gdp
+                    Header = "Test Header",
+                    Capital = "Test Capital",
+                    Population = 10, // Example value for Population
+                    Currency = "Test Currency",
+                    Area = 1000 // Example value for Area
+                };
+
+                // Add the new country to the DbSet
+                context.Countries.Add(newCountry);
+
+                // Save changes to the database
+                context.SaveChanges();
+            }
+        }
+
+        [Test]
+        public void CanRetrieveData()
+        {
+            using (var context = new ItcompanyDbContext(_options))
+            {
+                // Perform test queries or actions here
+                var companies = context.Companies.ToList();
+
+                Assert.IsNotNull(companies);
+                
+            }
+        }
+
+        // Add more tests as needed for other functionalities of your DbContext
+    }
+}
